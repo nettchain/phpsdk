d861288a2ae6b23c19ea9df3e8b13cc300295de9
Maicol Romero <michael12@hotmail.es>
2025-06-23 04:40:24 +0200

    Update NettChainClient.php

--- src/NettChainClient.php
+++ src/NettChainClient.php
@@ -66,6 +66,17 @@ class NettChainClient
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
         }
 
+
+        // Falta soporte para PUT, PATCH, DELETE
+        if ($method === 'DELETE') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        } elseif ($method === 'PUT') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        }
+
+        
+
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         
